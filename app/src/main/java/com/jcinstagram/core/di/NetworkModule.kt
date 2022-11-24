4dc485722e92370ad6a486b49aba6469e0ee3a21
Julian Cuestas Jimenez <julian.cuestas@sii-group.co>
2022-11-23 23:46:57 -0500

    Culminación inyección de dependencias con Dagger Hilt, capa data y domain, uso de Provides para inyección de retrofit e interfaces

---
+++ app/src/main/java/com/jcinstagram/core/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.jcinstagram.core.di
+
+import com.jcinstagram.login.data.network.LoginClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Alcance de la instancia autogenerada por Dagger Hilt, en este caso el singleton aplica para que tenga el alcance de app
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoginClient(retrofit: Retrofit): LoginClient {
+        return retrofit.create(LoginClient::class.java)
+    }
+}
--- app/src/main/java/com/jcinstagram/login/domain/LoginUseCase.kt
+++ app/src/main/java/com/jcinstagram/login/domain/LoginUseCase.kt
@@ -1,9 +1,9 @@
 package com.jcinstagram.login.domain
 
 import com.jcinstagram.login.data.LoginRepository
+import javax.inject.Inject
 
-class LoginUseCase {
-    private val repository = LoginRepository()
+class LoginUseCase @Inject constructor(private val repository: LoginRepository) {
 
     suspend operator fun invoke(user: String, password: String): Boolean {
         return repository.doLogin(user, password)
