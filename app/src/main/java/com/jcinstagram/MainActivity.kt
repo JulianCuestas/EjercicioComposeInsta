9a5edc725ccb612749a9a0418d093492baa63824
Julian Cuestas Jimenez <julian.cuestas@sii-group.co>
2022-11-23 19:56:54 -0500

    Primer commit, ejercicio compose con login (solo view) tipo instagram

---
+++ app/src/main/java/com/jcinstagram/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jcinstagram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.jcinstagram.login.LoginScreen
+import com.jcinstagram.ui.theme.JetpackComposeInstagramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeInstagramTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
